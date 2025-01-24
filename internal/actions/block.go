15429fe0627ee59315d434c5f0ce9793e393ca8d
Andre de Oliveira <oliveira.andrede@gmail.com>
2025-01-24 16:27:17 -0300

    Add small comment

--- internal/actions/block.go
+++ internal/actions/block.go
@@ -21,6 +21,8 @@ func (b *Block) Error(msg string) error {
 	return errors.New(msg)
 }
 
+// FIXME: we need a block for Punch, and Kick, they should be different! A block only happen if a
+// punch is set
 func (b *Block) Execute(g *game.Game, attacker *fighter.Fighter, defender *fighter.Fighter) error {
 	if defender.Stamina <= 0 || defender.Stamina < b.GetStaminaCost() {
 		return b.Error("Not enough stamina")
